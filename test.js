03990d99b01af9de6bb383ab17465b0b881cec13
Gage <77414756+Royuin@users.noreply.github.com>
2023-05-01 18:40:50 -0500

    added test for isSunk method returning ship sunk equaling false if hits are less than length

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-import { mock } from 'node:test';
 import {shipFactory} from './script';
 
 test('shipFactory object is marked as sunk when hits are equal to shipFactory length',() => {
@@ -8,3 +7,10 @@ test('shipFactory object is marked as sunk when hits are equal to shipFactory le
     mockShip.isSunk();
   expect(mockShip.sunk).toBe(true);
 }) 
+
+test('shipFactory object is marked as not sunk while hits are less than ship length', () => {
+  const mockShip = shipFactory(3);
+  mockShip.hit();
+  mockShip.isSunk();
+  expect(mockShip.sunk).toBe(false);
+})
