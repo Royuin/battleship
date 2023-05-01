3200132de81ed450f76bf787e9bc6949c29818a1
Gage <77414756+Royuin@users.noreply.github.com>
2023-05-01 17:36:53 -0500

    made test for shipFactory objects isSunk method to change ship sunk to true if hits match ship length

---
+++ test.js
@@ -0,0 +1,7 @@
+import {shipFactory} from './script';
+
+test('shipFactory object is marked as sunk when hit equal to as many times as shipFactory length',() => {
+  if (shipFactory.hits === shipFactory.length) {
+  expect(shipFactory.isSunk()).sunk.toBe(true)
+  }
+}) 
