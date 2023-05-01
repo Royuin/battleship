6bc341783879059154c16acd7e666d6ba4320015
Gage <77414756+Royuin@users.noreply.github.com>
2023-05-01 18:02:40 -0500

    cleaned up test for isSunk method returning true

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ import {shipFactory} from './script';
 
 test('shipFactory object is marked as sunk when hit equal to as many times as shipFactory length',() => {
   if (shipFactory.hits === shipFactory.length) {
-  expect(shipFactory.isSunk()).sunk.toBe(true)
-  }
+  expect(shipFactory.isSunk()).sunk.toBe(true);
+  } 
 }) 
