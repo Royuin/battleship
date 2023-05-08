a70a4d93e31a18a9d9dfb6f9da5567e5227fc066
Gage <77414756+Royuin@users.noreply.github.com>
2023-05-07 19:55:16 -0500

    made player factory with their own gameboard and attack method to attack other players board

--- script.js
+++ script.js
@@ -48,23 +48,22 @@ export function gameboardFactory() {
     j: [
       undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,
     ],
-
+    
     addShip: function (x, y, x2, y2) {
       if (x !== x2 && y !== y2) {
         throw new Error('Invalid Coordinates Soldier!');
       }
-     const thisShip = shipFactory(5)
-     this.ships.push(thisShip);
-     if (x === x2) {
-       for (let i = y-1 ; i < y2; i += 1 ) {
-         this[x][i] = thisShip;
+      const thisShip = shipFactory(5)
+      this.ships.push(thisShip);
+      if (x === x2) {
+        for (let i = y-1 ; i < y2; i += 1 ) {
+          this[x][i] = thisShip;
         }
       } else {
-         const rowArray = this.row;
-         const rowIndex1 = rowArray.indexOf(x);
-         const rowIndex2 = rowArray.indexOf(x2);
-
-         for (let i = rowIndex1; i <= rowIndex2; i += 1) {
+        const rowArray = this.row;
+        const rowIndex1 = rowArray.indexOf(x);
+        const rowIndex2 = rowArray.indexOf(x2); 
+        for (let i = rowIndex1; i <= rowIndex2; i += 1) {
           const value = rowArray[i];
           const currentRow = this[value];
           currentRow[y2 - 1] = thisShip;  
@@ -74,9 +73,7 @@ export function gameboardFactory() {
     receiveAttack: function(x, y) {
       if (this[x][y - 1] === undefined) {
         this[x][y - 1] = 'miss';
-      }
-
-     else {
+      } else {  
         this[x][y - 1].hit();
         this[x][y - 1] = 'hit';
         if (this.ships.every(ship => ship.sunk === true)) {
@@ -85,7 +82,17 @@ export function gameboardFactory() {
       }
     },
     ships: []
-
+    
   };
 }
 
+export function playerFactory(name) {
+  const gameboard = gameboardFactory();
+  return {
+    name,
+    gameboard,
+    attack: function(x,y ) {      
+      p2.gameboard.receiveAttack(x,y);
+    }
+  }
+}
