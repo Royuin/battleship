765b395bdc64e90bbe01eeead553fdba06ef69b4
Gage <77414756+Royuin@users.noreply.github.com>
2023-05-08 20:26:03 -0500

    made functions that add players boards to dom

--- dom.js
+++ dom.js
@@ -6,3 +6,27 @@ export function createRowCells(dom, array, row) {
     dom.appendChild(cell);
   }
 }
+
+export function displayP1Board(board) {
+  const boardsSection = document.querySelector('.boards');
+  const p1Grid = document.createElement('div');
+  p1Grid.classList = 'grid1';
+  boardsSection.appendChild(p1Grid)
+  for(let i = 0; i < board.row.length; i += 1) {
+    let index = board.row[i];
+    let row = board[index];
+    createRowCells(p1Grid, row, index)
+  }
+}
+
+export function displayCompBoard(board) {  
+  const boardsSection = document.querySelector('.boards');
+  const compGrid = document.createElement('div');
+  compGrid.classList = 'grid1';
+  boardsSection.appendChild(compGrid)
+  for(let i = 0; i < board.row.length; i += 1) {
+    let index = board.row[i];
+    let row = board[index];
+    createRowCells(compGrid, row, index)
+  }
+}
