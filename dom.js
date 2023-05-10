66a7188819692cf26044edd32656a2e456b81e5d
Gage <77414756+Royuin@users.noreply.github.com>
2023-05-09 19:10:19 -0500

    made displayColNums function and made display board functions put each board section in their own div

--- dom.js
+++ dom.js
@@ -7,23 +7,42 @@ export function createRowCells(dom, array, row) {
   }
 }
 
+ function displayColNums(playerGrid, boards) {
+  const row = document.createElement('div');
+  row.classList = 'num-row';
+  for (let i = 1; i <= 10; i += 1) {
+    const num = document.createElement('p');
+    num.textContent = i.toString();
+    row.appendChild(num);
+  }
+  playerGrid.insertBefore(row, boards)
+}
+
 export function displayP1Board(board) {
-  const boardsSection = document.querySelector('.boards');
+  const main = document.querySelector('main')
+  const board1 = document.createElement('div');
+  board1.classList = 'board1';
+  displayColNums(board1);
   const p1Grid = document.createElement('div');
   p1Grid.classList = 'grid1';
-  boardsSection.appendChild(p1Grid)
+  main.appendChild(board1);
+  board1.appendChild(p1Grid);
   for(let i = 0; i < board.row.length; i += 1) {
     let index = board.row[i];
     let row = board[index];
-    createRowCells(p1Grid, row, index)
+    createRowCells(p1Grid, row, index);
   }
 }
 
 export function displayCompBoard(board) {  
-  const boardsSection = document.querySelector('.boards');
+  const main = document.querySelector('main'); 
+  const board2 = document.createElement('div');
+  board2.classList = 'board2';
   const compGrid = document.createElement('div');
-  compGrid.classList = 'grid1';
-  boardsSection.appendChild(compGrid)
+  compGrid.classList = 'grid2';
+  main.appendChild(board2);
+  board2.appendChild(compGrid);
+  displayColNums(board2, compGrid);
   for(let i = 0; i < board.row.length; i += 1) {
     let index = board.row[i];
     let row = board[index];
