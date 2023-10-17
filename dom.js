6f7318e2e0d515ad9086b037310ce0caa9a5c5cf
Gage <77414756+Royuin@users.noreply.github.com>
2023-10-16 21:34:50 -0500

    Made a function to prevent the ends off new ships being added onto ships already on the board.

--- dom.js
+++ dom.js
@@ -6,11 +6,10 @@ export function createRowCells(dom, array, row) {
   }
 }
 
-
 export function displayP1Board(board) {
-  const main = document.querySelector('main')
+  const main = document.querySelector('main');
   if (document.querySelector('.board1')) {
-    main.removeChild(document.querySelector('.board1'))
+    main.removeChild(document.querySelector('.board1'));
   }
   const board1 = document.createElement('div');
   board1.classList = 'board1';
@@ -19,7 +18,7 @@ export function displayP1Board(board) {
   p1Grid.classList = 'grid1';
   main.appendChild(board1);
   board1.appendChild(p1Grid);
-  for(let i = 0; i < board.row.length; i += 1) {
+  for (let i = 0; i < board.row.length; i += 1) {
     let index = board.row[i];
     let row = board[index];
     const colLetter = document.createElement('p');
@@ -29,10 +28,10 @@ export function displayP1Board(board) {
   }
 }
 
-export function displayCompBoard(board) {  
+export function displayCompBoard(board) {
   const main = document.querySelector('main');
   if (document.querySelector('.board2')) {
-    main.removeChild(document.querySelector('.board2'))
+    main.removeChild(document.querySelector('.board2'));
   }
   const board2 = document.createElement('div');
   board2.classList = 'board2';
@@ -41,58 +40,58 @@ export function displayCompBoard(board) {
   main.appendChild(board2);
   board2.appendChild(compGrid);
   displayColNums(board2, compGrid);
-  for(let i = 0; i < board.row.length; i += 1) {
+  for (let i = 0; i < board.row.length; i += 1) {
     let index = board.row[i];
     let row = board[index];
     const colLetter = document.createElement('p');
     colLetter.textContent = board.row[i];
     compGrid.appendChild(colLetter);
-    createRowCells(compGrid, row, index)
+    createRowCells(compGrid, row, index);
   }
 }
 
 function displayColNums(playerGrid, boards) {
- const row = document.createElement('div');
- row.classList = 'num-row';
- const blank = document.createElement('p');
- row.appendChild(blank)
- for (let i = 1; i <= 10; i += 1) {
-   const num = document.createElement('p');
-   num.textContent = i.toString();
-   row.appendChild(num);
- }
- playerGrid.insertBefore(row, boards)
+  const row = document.createElement('div');
+  row.classList = 'num-row';
+  const blank = document.createElement('p');
+  row.appendChild(blank);
+  for (let i = 1; i <= 10; i += 1) {
+    const num = document.createElement('p');
+    num.textContent = i.toString();
+    row.appendChild(num);
+  }
+  playerGrid.insertBefore(row, boards);
 }
 
-export function updateDomBoard(p1,p2) {
+export function updateDomBoard(p1, p2) {
   const letterArray = p1.gameboard.row;
 
-  let p1Cells = document.querySelectorAll('.grid1 > .cell')
-  for (let l = 0; l < letterArray.length; l += 1 ) {
+  let p1Cells = document.querySelectorAll('.grid1 > .cell');
+  for (let l = 0; l < letterArray.length; l += 1) {
     let thisLetter = letterArray[l];
     for (let n = 0; n < 10; n += 1) {
       const value = p1.gameboard[thisLetter][n];
-      const thisCell = p1Cells[(l * 10) + n];    
+      const thisCell = p1Cells[l * 10 + n];
       if (typeof value === 'object') {
-        thisCell.classList = ' cell ship'
+        thisCell.classList = ' cell ship';
       } else if (value === 'hit') {
-          thisCell.classList = ' cell hit';
+        thisCell.classList = ' cell hit';
       } else if (value === 'miss') {
-          thisCell.classList = ' cell miss';
-        } 
+        thisCell.classList = ' cell miss';
       }
     }
-  
-    let p2Cells = document.querySelectorAll('.grid2 > .cell')
-    for (let l = 0; l < letterArray.length; l += 1 ) {
-      let thisLetter = letterArray[l];
-      for (let n = 0; n < 10; n += 1) {
-        const value = p2.gameboard[thisLetter][n];
-      const thisCell = p2Cells[(l * 10) + n];
+  }
+
+  let p2Cells = document.querySelectorAll('.grid2 > .cell');
+  for (let l = 0; l < letterArray.length; l += 1) {
+    let thisLetter = letterArray[l];
+    for (let n = 0; n < 10; n += 1) {
+      const value = p2.gameboard[thisLetter][n];
+      const thisCell = p2Cells[l * 10 + n];
       if (value === 'hit') {
         thisCell.classList = ' cell hit';
       } else if (value === 'miss') {
-          thisCell.classList = ' cell miss';
+        thisCell.classList = ' cell miss';
       }
     }
   }
@@ -102,11 +101,11 @@ export function attackQuerySelectors(p1, p2) {
   const cellList = document.querySelectorAll('.grid2 > .cell');
   for (let i = 0; i < cellList.length; i += 1) {
     let currentCell = cellList[i];
-    
+
     currentCell.addEventListener('click', () => {
       if (i < 10) {
         p1.attack('a', i + 1);
-      } else if (i < 20 ) {
+      } else if (i < 20) {
         p1.attack('b', i - 10 + 1);
       } else if (i < 30) {
         p1.attack('c', i - 20 + 1);
@@ -122,23 +121,23 @@ export function attackQuerySelectors(p1, p2) {
         p1.attack('h', i - 70 + 1);
       } else if (i < 90) {
         p1.attack('i', i - 80 + 1);
-      } else if (i < 100) { 
+      } else if (i < 100) {
         p1.attack('j', i - 90 + 1);
       }
-      updateDomBoard(p1, p2)
+      updateDomBoard(p1, p2);
     });
   }
 }
 
 export function updateWinner(player) {
-  const winnerMessage = document.querySelector('.winner-message')
+  const winnerMessage = document.querySelector('.winner-message');
   if (!player) {
     winnerMessage.textContent = '';
   } else if (player.name === 'player') {
     winnerMessage.textContent = 'YOU WIN!';
   } else if (player.name === 'computer') {
     winnerMessage.textContent = 'YOU LOSE!';
-  } 
+  }
 }
 
 export function addShipListeners(p1, p2) {
@@ -146,45 +145,54 @@ export function addShipListeners(p1, p2) {
 
   for (let i = 0; i < cellList.length; i += 1) {
     let currentCell = cellList[i];
-    let newShipLength
+    let newShipLength;
 
     currentCell.addEventListener('mouseenter', () => {
       if (p1.gameboard.ships.length === 0) {
-        newShipLength = 4
-      }  else if (p1.gameboard.ships.length > 0 && p1.gameboard.ships.length < 3 ) {
-        newShipLength = 3      
-      } else if (p1.gameboard.ships.length > 2 && p1.gameboard.ships.length <= 5 ) {
+        newShipLength = 4;
+      } else if (
+        p1.gameboard.ships.length > 0 &&
+        p1.gameboard.ships.length < 3
+      ) {
+        newShipLength = 3;
+      } else if (
+        p1.gameboard.ships.length > 2 &&
+        p1.gameboard.ships.length <= 5
+      ) {
         newShipLength = 2;
-      }  else if (p1.gameboard.ships.length > 5 && p1.gameboard.ships.length < 10) {
+      } else if (
+        p1.gameboard.ships.length > 5 &&
+        p1.gameboard.ships.length < 10
+      ) {
         newShipLength = 1;
       } else if (p1.gameboard.ships.length >= 10) {
         return;
       }
-    
-      let thisCellList = [currentCell,];
-      for (let n = 1; n < newShipLength; n +=1) {
+
+      let thisCellList = [currentCell];
+      for (let n = 1; n < newShipLength; n += 1) {
         if (i + n >= 100) {
           return;
         } else {
-        thisCellList.push(cellList[i + n]);
+          thisCellList.push(cellList[i + n]);
         }
-      }   
-      
-      thisCellList.forEach(cell => {
-        cell.classList.add('hovered')
+      }
+
+      thisCellList.forEach((cell) => {
+        cell.classList.add('hovered');
       });
 
       currentCell.addEventListener('mouseleave', () => {
-       thisCellList.forEach(cell => {
-        cell.classList.remove('hovered');
-       }) 
-      })
-    })
+        thisCellList.forEach((cell) => {
+          cell.classList.remove('hovered');
+        });
+      });
+    });
 
     currentCell.addEventListener('click', () => {
       if (i < 10) {
         p1.gameboard.addShip('a', i + 1);
-      } else if (i < 20 ) {
+      } else if (i < 20) {
         p1.gameboard.addShip('b', i - 10 + 1);
       } else if (i < 30) {
         p1.gameboard.addShip('c', i - 20 + 1);
@@ -197,64 +205,73 @@ export function addShipListeners(p1, p2) {
       } else if (i < 70) {
         p1.gameboard.addShip('g', i - 60 + 1);
       } else if (i < 80) {
-        p1.gameboard.addShip('h', i -70 + 1);
+        p1.gameboard.addShip('h', i - 70 + 1);
       } else if (i < 90) {
-        p1.gameboard.addShip('i', i -80 + 1);
+        p1.gameboard.addShip('i', i - 80 + 1);
       } else if (i < 100) {
-        p1.gameboard.addShip('j', i -90 + 1);
-      } 
-      updateDomBoard(p1, p2)
-    }); 
+        p1.gameboard.addShip('j', i - 90 + 1);
+      }
+      updateDomBoard(p1, p2);
+    });
   }
 }
 
 export function addShipListenersHorizontal(p1, p2) {
   const cellList = document.querySelectorAll('.grid1 > .cell');
-  
+
   for (let i = 0; i < cellList.length; i += 1) {
     let currentCell = cellList[i];
-    let newShipLength
+    let newShipLength;
 
     currentCell.addEventListener('mouseenter', () => {
       if (p1.gameboard.ships.length === 0) {
-        newShipLength = 4
-      }  else if (p1.gameboard.ships.length > 0 && p1.gameboard.ships.length < 3 ) {
-        newShipLength = 3      
-      } else if (p1.gameboard.ships.length > 2 && p1.gameboard.ships.length <= 5 ) {
+        newShipLength = 4;
+      } else if (
+        p1.gameboard.ships.length > 0 &&
+        p1.gameboard.ships.length < 3
+      ) {
+        newShipLength = 3;
+      } else if (
+        p1.gameboard.ships.length > 2 &&
+        p1.gameboard.ships.length <= 5
+      ) {
         newShipLength = 2;
-      }  else if (p1.gameboard.ships.length > 5 && p1.gameboard.ships.length < 10) {
+      } else if (
+        p1.gameboard.ships.length > 5 &&
+        p1.gameboard.ships.length < 10
+      ) {
         newShipLength = 1;
       } else if (p1.gameboard.ships.length >= 10) {
         return;
       }
-    
+
       let thisCellList = [currentCell];
-      for (let n = 1; n < newShipLength; n +=1) {
+      for (let n = 1; n < newShipLength; n += 1) {
         if (i + n >= 100) {
           return;
         } else {
-        thisCellList.push(cellList[i + n * 10]);
+          thisCellList.push(cellList[i + n * 10]);
         }
-      }   
-      
-      cellList.forEach(cell => {
-        cell.classList.remove('hovered'); 
-      })
+      }
 
-      thisCellList.forEach(cell => {
-        cell.classList.add('hovered')
+      cellList.forEach((cell) => {
+        cell.classList.remove('hovered');
+      });
+
+      thisCellList.forEach((cell) => {
+        cell.classList.add('hovered');
       });
 
       currentCell.addEventListener('mouseleave', () => {
-       thisCellList.forEach(cell => {
-        cell.classList.remove('hovered');
-       }) 
-      })
-    })
+        thisCellList.forEach((cell) => {
+          cell.classList.remove('hovered');
+        });
+      });
+    });
     currentCell.addEventListener('click', () => {
       if (i < 10) {
-        p1.gameboard.addShip('a',i + 1, true);
-      } else if (i < 20 ) {
+        p1.gameboard.addShip('a', i + 1, true);
+      } else if (i < 20) {
         p1.gameboard.addShip('b', i - 10 + 1, true);
       } else if (i < 30) {
         p1.gameboard.addShip('c', i - 20 + 1, true);
@@ -267,14 +284,13 @@ export function addShipListenersHorizontal(p1, p2) {
       } else if (i < 70) {
         p1.gameboard.addShip('g', i - 60 + 1, true);
       } else if (i < 80) {
-        p1.gameboard.addShip('h', i -70 + 1, true);
+        p1.gameboard.addShip('h', i - 70 + 1, true);
       } else if (i < 90) {
-        p1.gameboard.addShip('i', i -80 + 1, true);
+        p1.gameboard.addShip('i', i - 80 + 1, true);
       } else if (i < 100) {
-        p1.gameboard.addShip('j', i -90 + 1, true);
-      } 
-      updateDomBoard(p1, p2)
-    }); 
+        p1.gameboard.addShip('j', i - 90 + 1, true);
+      }
+      updateDomBoard(p1, p2);
+    });
   }
 }
-
--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-import { displayCompBoard, displayP1Board, updateDomBoard, attackQuerySelectors, updateWinner, addShipListeners, addShipListenersHorizontal } from "./dom.js";
+import {
+  displayCompBoard,
+  displayP1Board,
+  updateDomBoard,
+  attackQuerySelectors,
+  updateWinner,
+  addShipListeners,
+  addShipListenersHorizontal,
+} from './dom.js';
 
 export function shipFactory(length) {
   return {
@@ -7,7 +15,7 @@ export function shipFactory(length) {
     sunk: false,
     isSunk: function () {
       if (this.hits === this.length) {
-        this.sunk = true;  
+        this.sunk = true;
       }
     },
     hit: function () {
@@ -23,72 +31,158 @@ export function gameboardFactory() {
     ships: [],
     row: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'],
     a: [
-      undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
     ],
     b: [
-      undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
     ],
     c: [
-      undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
     ],
     d: [
-      undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
     ],
     e: [
-      undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
     ],
     f: [
-      undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
     ],
     g: [
-      undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
     ],
     h: [
-      undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
     ],
     i: [
-      undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
     ],
     j: [
-      undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
     ],
-    
+
     addShip: function (x, y, horizontal) {
       if (this.ships.length === 0) {
-        newShip = shipFactory(4)
-      }  else if (this.ships.length > 0 && this.ships.length < 3 ) {
-        newShip = shipFactory(3)      
-      } else if (this.ships.length > 2 && this.ships.length < 5 ) {
+        newShip = shipFactory(4);
+      } else if (this.ships.length > 0 && this.ships.length < 3) {
+        newShip = shipFactory(3);
+      } else if (this.ships.length > 2 && this.ships.length < 5) {
         newShip = shipFactory(2);
-      }  else if (this.ships.length > 5 && this.ships.length < 10) {
+      } else if (this.ships.length > 5 && this.ships.length < 10) {
         newShip = shipFactory(1);
       } else if (this.ships.length >= 10) {
         throw new Error("You're out of ships soldier");
       }
-      
-      if (!horizontal && y - 1 + newShip.length > 10) {
-       return new Error('Invalid Coordinates Soldier!');
-      } else if (horizontal && this.row.indexOf(x) + newShip.length > 10) {
+
+      if (this.invalidCoordinates(x, y, newShip.length, horizontal) === true) {
         return new Error('Invalid Coordinates Soldier!');
       }
 
       if (newShip.length === 1) {
         if (this[x][y - 1] !== undefined) {
           return new Error('Coordinates already attacked!');
-        }
-        else {
-         this[x][y - 1] = newShip;
-         this.ships.push(newShip);
+        } else {
+          this[x][y - 1] = newShip;
+          this.ships.push(newShip);
         }
       } else if (!horizontal) {
         for (let i = 0; i < newShip.length; i += 1) {
-          let yIndex = y + i - 1 
-          if ( this[x][yIndex] !== undefined) {
+          if (this[x][y + i - 1] !== undefined) {
             return new Error('Coordinates already attacked!');
-          } 
-          this[x][y + i - 1] = newShip;
           }
-          this.ships.push(newShip);
+          this[x][y + i - 1] = newShip;
+        }
+        this.ships.push(newShip);
       } else if (horizontal) {
         let xIndex = this.row.indexOf(x);
         for (let i = 0; i < newShip.length; i += 1) {
@@ -96,22 +190,47 @@ export function gameboardFactory() {
           if (i === 0) {
             newIndex = xIndex;
           } else {
-          newIndex = xIndex + i;
+            newIndex = xIndex + i;
           }
           let newX = this.row[newIndex];
-          if (this[newX][y - 1] !== undefined) {
+          if (this[newX][y] !== undefined) {
             return new Error('Coordinate already attacked!');
-          } else  {
-          this[x][y-1] = newShip;
-          this[newX][y - 1] = newShip;
+          } else {
+            this[x][y - 1] = newShip;
+            this[newX][y - 1] = newShip;
           }
         }
         this.ships.push(newShip);
       }
-    }
-  ,
-    receiveAttack: function(x, y, p2Attack) {
-      if (this.ships.every(ship => ship.sunk === true)) {
+    },
+    invalidCoordinates(x, y, shipLength, horizontal) {
+      if (!horizontal) {
+        if (y - 1 + newShip.length > 10) {
+          return true;
+        }
+        for (let i = 0; i < shipLength; i += 1) {
+          if (this[x][y + i - 1] !== undefined) {
+            return true;
+          }
+        }
+      } else {
+        if (horizontal && this.row.indexOf(x) + newShip.length > 10) {
+          console.log('ERROR');
+          return TextTrackCueList;
+        }
+        for (let i = 0; i < shipLength; i += 1) {
+          let xIndex = this.row.indexOf(x) + i;
+          let updatedX = this.row[xIndex];
+
+          if (this[updatedX][y - 1] !== undefined) {
+            console.log('ERROR');
+            return true;
+          }
+        }
+      }
+    },
+    receiveAttack: function (x, y, p2Attack) {
+      if (this.ships.every((ship) => ship.sunk === true)) {
         if (p2Attack) {
           gameOver = true;
           updateWinner(p1);
@@ -119,30 +238,30 @@ export function gameboardFactory() {
           updateWinner(p2);
           gameOver = true;
         }
-        return
+        return;
       } else if (this[x][y - 1] === undefined) {
-          this[x][y - 1] = 'miss';
-          if (p2Attack) {
-            p2.attack();
-          }
-      } else if (this[x][y-1] === 'hit' || this[x][y-1] === 'miss') {    
-        if (!p2Attack) {
-          p2.attack()
-        } else {
-       return new Error('Coordinates already attacked!'); 
+        this[x][y - 1] = 'miss';
+        if (p2Attack) {
+          p2.attack();
         }
-      } else if (typeof this[x][y - 1] === 'object' ){  
-          this[x][y - 1].hit();
-          this[x][y - 1] = 'hit';
-           if (p2Attack){
-            p2.attack();
-          }
+      } else if (this[x][y - 1] === 'hit' || this[x][y - 1] === 'miss') {
+        if (!p2Attack) {
+          p2.attack();
+        } else {
+          return new Error('Coordinates already attacked!');
+        }
+      } else if (typeof this[x][y - 1] === 'object') {
+        this[x][y - 1].hit();
+        this[x][y - 1] = 'hit';
+        if (p2Attack) {
+          p2.attack();
+        }
       }
     },
-    fillBoard: function() {
-      while (this.ships.length < 10) { 
-        const horizontal = Math.round(Math.random() * 1 + 0);  
-        let letter = this.row[Math.floor(Math.random() *  this.row.length)]
+    fillBoard: function () {
+      while (this.ships.length < 10) {
+        const horizontal = Math.round(Math.random() * 1 + 0);
+        let letter = this.row[Math.floor(Math.random() * this.row.length)];
         let num = Math.floor(Math.random() * 10 + 1);
         if (horizontal === 0) {
           this.addShip(letter, num);
@@ -156,28 +275,29 @@ export function gameboardFactory() {
 
 export function playerFactory(name) {
   const gameboard = gameboardFactory();
-  let attack = function(x,y ) {      
-    p2.gameboard.receiveAttack(x,y, p2.attack);
+  let attack = function (x, y) {
+    p2.gameboard.receiveAttack(x, y, p2.attack);
   };
   if (name === 'computer') {
     attack = function () {
-      let compX = gameboard.row[Math.floor(Math.random() * gameboard.row.length)]
+      let compX =
+        gameboard.row[Math.floor(Math.random() * gameboard.row.length)];
       let compY = Math.floor(Math.random() * 10 + 1);
-      p1.gameboard.receiveAttack(compX,compY);
+      p1.gameboard.receiveAttack(compX, compY);
     };
-  };
+  }
   return {
     name,
     gameboard,
     attack,
-  }
+  };
 }
 
 export const main = document.querySelector('main');
 
 let p1 = playerFactory('player');
 let p1Board = p1.gameboard;
-let p2 = playerFactory('computer')
+let p2 = playerFactory('computer');
 let p2Board = p2.gameboard;
 let gameOver = false;
 
@@ -191,35 +311,35 @@ const axisBtn = document.querySelector('.axis-button');
 startListener.addEventListener('click', () => {
   if (p1.gameboard.ships.length < 10) {
     return alert('You need to add your ships first!');
-  } else if (gameOver) { 
-    p1 = playerFactory('player'); 
+  } else if (gameOver) {
+    p1 = playerFactory('player');
     p2 = playerFactory('computer');
     displayP1Board(p1.gameboard);
     displayCompBoard(p2.gameboard);
-    addShipListeners(p1,p2);
+    addShipListeners(p1, p2);
     gameOver = false;
     updateWinner();
   } else {
     p2.gameboard.fillBoard();
-  displayP1Board(p1.gameboard);
-  displayCompBoard(p2.gameboard);
-  updateDomBoard(p1,p2);
-  attackQuerySelectors(p1, p2);
+    displayP1Board(p1.gameboard);
+    displayCompBoard(p2.gameboard);
+    updateDomBoard(p1, p2);
+    attackQuerySelectors(p1, p2);
   }
-})
+});
 
 axisBtn.addEventListener('click', () => {
-  if (p1.gameboard.ships.length ===  10) {
+  if (p1.gameboard.ships.length === 10) {
     return;
   }
   displayP1Board(p1Board);
   displayCompBoard(p2Board);
-  updateDomBoard(p1,p2);
+  updateDomBoard(p1, p2);
   if (axisBtn.textContent === 'Axis: Y') {
-    addShipListenersHorizontal(p1, p2); 
+    addShipListenersHorizontal(p1, p2);
     axisBtn.textContent = 'Axis: X';
   } else if (axisBtn.textContent === 'Axis: X') {
-    addShipListeners(p1,p2);
+    addShipListeners(p1, p2);
     axisBtn.textContent = 'Axis: Y';
   }
-})
+});
